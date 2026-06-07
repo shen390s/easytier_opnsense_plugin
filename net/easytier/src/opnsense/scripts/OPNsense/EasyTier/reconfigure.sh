d16b068dae0d8b05145cefa5ed486472e89b7ee0
Rongsong Shen <rshen@shenrs.eu>
2026-06-07 17:29:21 +0800

    fix firewall issue

--- net/easytier/src/opnsense/scripts/OPNsense/EasyTier/reconfigure.sh
+++ net/easytier/src/opnsense/scripts/OPNsense/EasyTier/reconfigure.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Regenerate the configuration template
-/usr/local/bin/configctl template reload OPNsense/EasyTier
+/usr/local/sbin/configctl template reload OPNsense/EasyTier
 
 # Register the EasyTier interface in OPNsense config
-/usr/local/bin/configctl interface invoke registration
+/usr/local/sbin/configctl interface invoke registration
 
 # Check if EasyTier is enabled in the configuration
 ENABLED=$(/usr/local/sbin/pluginctl -g OPNsense.EasyTier.general.enabled 2>/dev/null)
@@ -16,15 +16,15 @@ if [ "${ENABLED}" = "1" ]; then
     /usr/local/etc/rc.d/easytier start
     # Wait for TUN interface to come up
     sleep 3
-    # Add the fixed easytier0 interface to the easytier group
+    # Add the easytier0 interface to the easytier group
     if ifconfig easytier0 >/dev/null 2>&1; then
         ifconfig easytier0 group easytier 2>/dev/null
     fi
-    # Reload firewall to apply rules
-    /usr/local/bin/configctl filter reload
+    # Reload firewall to apply pass rules from easytier_firewall() in easytier.inc
+    /usr/local/sbin/configctl filter reload
 else
     /usr/local/etc/rc.d/easytier stop 2>/dev/null
     pkill -9 easytier-core 2>/dev/null
     ifconfig easytier0 -group easytier 2>/dev/null
-    /usr/local/bin/configctl filter reload
+    /usr/local/sbin/configctl filter reload
 fi
